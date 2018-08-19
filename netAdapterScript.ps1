499dcd4670563a25b6bbf69a2af3220ca9275c3a
piscesmillennial <35496470+piscesmillennial@users.noreply.github.com>
2018-08-19 13:51:23 -0400

    Reboot Network Adapter
    This PowerShell script displays all available network adapters in the Windows Network Connections dialog and provides a quick way to reboot a selected adapter in case of network connection issues.

---
+++ netAdapterScript.ps1
@@ -0,0 +1,45 @@
+﻿
+#Set-ExecutionPolicy RemoteSigned
+
+$looping = $true
+
+do{
+
+
+Write-Host ('')
+
+$adapterList = Get-NetAdapter -Name '*'
+
+for($i=0; $i -lt $adapterList.Length; $i++)
+{
+    write-host ([System.String]::Concat($i+1, ". ", $adapterList[$i].Name, ' -------> ',  $adapterList[$i].Status, ' -------> ',  $adapterList[$i].Speed) )
+}
+
+Write-Host ('')
+
+
+$selection = Read-Host('Select a number to restart a network adapter, or press q/Q to quit.')
+
+if($selection -notmatch "([Qq0-9])"){
+    Write-Host ('')
+    Write-Host ('**Bad input - Please select a number or press q/Q to quit, try again.**')
+
+}elseif($selection -eq 'q' -or $selection -eq 'Q'){
+    Exit
+
+}elseif($selection -in 1..$adapterList.Count){
+    $netObject = $adapterList[$selection-1]
+    Write-Host ('Restarting network adapter ' + $netObject.ifAlias)    
+    
+    Write-hOST "Working..."
+    Restart-NetAdapter -Name $netObject.ifAlias    
+    
+    Start-Sleep -s 5
+}
+
+
+}While($looping)
+
+
+
+
